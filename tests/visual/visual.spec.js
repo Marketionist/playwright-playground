b6f4bfa7e0cf93e49c62c332fe87374aaf33ce05
Marketionist <marketionist@gmail.com>
2022-09-11 08:59:39 -0400

    Add visual regression tests

---
+++ tests/visual/visual.spec.js
@@ -0,0 +1,46 @@
+const { test, expect } = require('@playwright/test');
+// With Page Object
+const { MainPage } = require('../../page-objects/main-page.js');
+
+test.describe('Visual regression test examples', () => {
+    test('Full page snapshot', async ({ page }) => {
+        await page.goto('https://playwright.dev/');
+
+        // See https://github.com/microsoft/playwright/issues/10219#issuecomment-1062120505 and
+        // https://playwright.dev/docs/test-assertions#page-assertions-to-have-screenshot-2
+        // for all toHaveScreenshot options
+        expect(await page.screenshot()).toMatchSnapshot('homepage.png');
+    });
+
+    test('Full page snapshot (with Page Object)', async ({ page }) => {
+        const mainPage = new MainPage(page);
+
+        await mainPage.goTo('https://playwright.dev');
+
+        expect(await page.screenshot()).toMatchSnapshot('homepage.png');
+    });
+
+    test('Single element snapshot', async ({ page }) => {
+        await page.goto('https://playwright.dev');
+
+        const pageTitle = await page.$('h1');
+
+        // See https://playwright.dev/docs/test-assertions#screenshot-assertions-to-match-snapshot-1
+        // for all toMatchSnapshot options
+        expect(await pageTitle.screenshot()).toMatchSnapshot('page-title.png', { maxDiffPixels: 20 });
+    });
+
+    test('Single element snapshot (with Page Object)', async ({ page }) => {
+        const mainPage = new MainPage(page);
+
+        await mainPage.verifyPageTitle('https://playwright.dev');
+
+        expect(await mainPage.pageTitle.screenshot()).toMatchSnapshot('page-title.png', { maxDiffPixels: 20 });
+    });
+
+    test('Text content comparison', async ({ page }) => {
+        await page.goto('https://playwright.dev');
+
+        expect(await page.textContent('.hero__title')).toMatchSnapshot('hero.txt');
+    });
+});
