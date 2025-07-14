241b6c1a3a8ad183616aa794b7a0d35c4bc9a47e
Marketionist <marketionist@gmail.com>
2025-07-14 08:29:52 -0400

    Add setup scripts

---
+++ tests/e2e/setup.js
@@ -0,0 +1,36 @@
+const { test, expect } = require('@playwright/test');
+const mainPage = require('../../page-objects/e2e-main-page.js');
+
+test.describe('Setup Para Bank test data', () => {
+    let credentials;
+
+    test.beforeEach(async ({ context, }) => {
+        credentials = await mainPage.getCredentialsFromCookies(context);
+        // await page.goto('http://zero.webappsecurity.com/');
+    });
+
+    test('POST /user - create new user.', async ({ context }) => {
+        console.log('\ncredentials in setup.js:', credentials);
+        // const createUserResponse = await context.request.post(
+        //     `${mainPage.baseUrl}/users`,
+        //     {
+        //         params: {
+        //             parameter_one: 'username',
+        //             parameter_two: 'password'
+        //         },
+        //         headers: {
+        //             'x-api-key': 'reqres-free-v1'
+        //         },
+        //         data: {
+        //             'email': 'eve.holt@reqres.in',
+        //             'password': 'cityslicka'
+        //         }
+        //     }
+        // );
+        // const createUserResponseBody = JSON.parse(await createUserResponse.text());
+    
+        // expect(createUserResponse.status()).toBe(200);
+        // expect(createUserResponseBody.token).toBeTruthy();
+    });
+
+});
