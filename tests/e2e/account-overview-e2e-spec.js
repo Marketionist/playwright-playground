682145fa59597ac08a6917705f0bec2cbc6c7d83
Marketionist <marketionist@gmail.com>
2025-07-23 00:20:47 -0400

    Switch e2e tests to Para Bank

---
+++ tests/e2e/account-overview-e2e-spec.js
@@ -0,0 +1,17 @@
+const { test, expect } = require('@playwright/test');
+const mainPage = require('../../page-objects/e2e-main-page.js');
+const { getCredentialsFromCookies } = require('../../utils/log-in.js');
+
+test.describe.parallel('Account Overview', () => {
+    let credentials;
+
+    test.beforeEach(async ({ page, context }) => {
+        credentials = await getCredentialsFromCookies(context);
+        await page.goto(`${mainPage.baseUrl}/overview.htm`);
+    });
+
+    test('Verify Accounts Overview table', async ({ page }) => {
+        await expect(page.locator(mainPage.tableAccountsOverview)).toBeVisible();
+    });
+
+});
