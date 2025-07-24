c41b910c686dd2ba4829ddb3e580e69b729dd6ad
Marketionist <marketionist@gmail.com>
2025-07-24 00:43:45 -0400

    Add test for Transfer Funds

---
+++ tests/e2e/transfer-funds-e2e-spec.js
@@ -0,0 +1,26 @@
+const { test, expect } = require('@playwright/test');
+const mainPage = require('../../page-objects/e2e-main-page.js');
+const { getCredentialsFromCookies } = require('../../utils/log-in.js');
+
+test.describe.parallel.only('Transfer Funds', () => {
+    let credentials;
+
+    test.beforeEach(async ({ page, context }) => {
+        credentials = await getCredentialsFromCookies(context);
+        await page.goto(`${mainPage.baseUrl}/transfer.htm`);
+    });
+
+    test('Verify money transfer', async ({ page }) => {
+        await page.locator(mainPage.inputAmount).fill('100');
+        await page.locator(mainPage.buttonTransfer).click();
+
+        // // Wait for 5 seconds
+        // await page.waitForTimeout(5000);
+
+        await expect(page.locator(mainPage.titleResult)).toBeVisible();
+        await expect(page.locator(mainPage.titleResult)).toContainText(
+            'Transfer Complete!'
+        );
+    });
+
+});
